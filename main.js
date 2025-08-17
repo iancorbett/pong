f9b697dd09349896abc3062a641547c83a47c33c
iancorbett <iancorbett1324@gmail.com>
2025-08-16 22:48:47 -0700

    use necessary consts by importn g from html file using the document.getelement by id

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d"); //allows us to draw on canvas
+const p1ScoreEl = document.getElementById("p1Score");
+const p2ScoreEl = document.getElementById("p2Score");
+const pauseBtn = document.getElementById("pauseBtn");
+const restartBtn = document.getElementById("restartBtn");
+const twoPlayerChk = document.getElementById("twoPlayer");
